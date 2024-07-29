f26953050572b1053fb18019be0bf8f7a8103fe9
Maksym Khanin <223687@hk.fwd.com>
2024-07-29 20:51:30 +0800

    created shopping cards

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import jeans from'./jeans3.jpg';
+import {useEffect, useState} from 'react';
 
 function App() {
+  const [items, setItems] = useState([]);
+  console.log("Rendering with: ", items);
+
+  useEffect(()=>{
+    fetchItems();
+  },[]);
+
+  async function fetchItems() {
+    const response = await fetch("https://fakestoreapi.com/products");
+    const json = await response.json();
+
+    setItems([...items, ...json]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h2 id='nav-title'>
+          E-Shop
+        </h2>
+        <nav>
+          <ul>
+            <li><a href='#'>About</a></li>
+            <li><a href='#'>Services</a></li>
+            <li><a href='#'>Contact</a></li>
+          </ul>
+        </nav>
       </header>
-    </div>
+      <div className='main_block'>
+      {items && items.map((item, index) => (
+        <div key={index} className='shopping_card'>
+          <img className='image' src={item.image} alt="Denim Jeans"></img>
+          <h3>{item.title.split(' ').slice(0,3).join(' ')}</h3>
+          <p className="price">${item.price}</p>
+          <p><button>Add to Cart</button></p>
+      </div>
+      ))}
+        
+      </div>
+    </>
   );
 }
 
